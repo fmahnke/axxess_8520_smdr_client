91052a8ec95d6d711ea9e241f1558f249fe6ea2e
Fritz Mahnke <fritz@fritzmahnke.com>
2012-07-09 16:55:46 -0500

    Add a test database and some config detail

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 import logging
 
-DATABASE_NAME = '' # sqlite database file
-AXXESS_IP = ""
+# Absolute path to sqlite database file e.g. sqdatabase.db
+DATABASE_NAME = ''
+# Quoted IP address of Axxess PBX system. Needed for TCP/IP client.
+AXXESS_IP = ''
+# TCP connection port. Needed for TCP/IP client.
 AXXESS_PORT = 4000
 
-LOG_FILE = '' # file to write log messages
+# Absolute path of file to write log messages. Leave blank ('') for none.
+LOG_FILE = 'testlog.log'
 
+# If LOG_FILE is defined, define the format of the log messages to write.
 if (LOG_FILE is not None):
     logging.basicConfig(format = '%(asctime)s %(message)s',
                         filename = LOG_FILE,
